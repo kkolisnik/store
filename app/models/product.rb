6bec50f8e5f87e2c46bfda033296dc1d655d7efc
Kelly Kolisnik <kkolisnik@academic.rrc.ca>
2012-12-04 14:30:33 -0600

    Store Final Complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
-  attr_accessible :category, :description, :id, :image, :price, :product, :product_number, :sale, :sale_price
+  attr_accessible :category, :description,  :image, :price, :product, :product_number, :sale, :sale_price
   validates_inclusion_of :category, :in => ['Mens', 'Womens']
-  
-  validates_presence_of :id, :product, :price, :category, :image
-
+  #
+  validates_presence_of :product, :price, :category, :image
+  #
   validates_uniqueness_of :product_number
   
 end
+
